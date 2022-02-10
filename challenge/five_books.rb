71f49cc1ed558bebd0cb6bb21eab0c042d10b9ff
Caden Jarrett <caden.jarrett99@gmail.com>
2022-02-10 09:42:48 -0700

    secon commit, may have not saved correctly originally

---
+++ challenge/five_books.rb
@@ -0,0 +1,8 @@
+genre = "fantasy" # we chose it based on prior instructions.
+size = "large"
+number_of_books = 2572
+quick_reads = false
+exciting_reads = !false
+avg_num_pages = 400.48
+
+alternate_genres = ["literary fiction", "nonfiction", "science fiction", "YA"]
---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+# List of Books
+1. Harry Potter
+2. Percy Jackson
+3. Chaos
+4. Lord of the Rings
+5. The Bible
